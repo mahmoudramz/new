13afec5342e859bdb10e32a5f9be52978c7fd819
mahmoudramz <52760874+mahmoudramz@users.noreply.github.com>
2019-08-06 17:31:31 -0400

    Add files via upload

---
+++ assets/data.ts
@@ -0,0 +1,6 @@
+import { myProfile } from '../app/myProfile';
+
+export const MYPROFILE : myProfile = {
+    id : '999999', name : 'Mahmoud' , year: 4
+};
+
---
+++ assets/mycourses.ts
@@ -0,0 +1,8 @@
+import { Course} from '../app/courses';
+
+export const MYCOURSES: Course[] = [
+{ cid :'98987SYS', cname:'Mobile Web', cprof:'Andy'},
+{ cid :'98987SYS', cname:'Mobile Web', cprof:'Andy'}
+
+];
+
